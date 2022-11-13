00a67443de23ca96873aeb035b280857f98f6336
afodor88 <afodor88@gmail.com>
2022-11-13 16:42:02 +0200

    moving the loseCount logic after the loop so it calculate correctly win/loses;

--- src/com/company/DiceGame.java
+++ src/com/company/DiceGame.java
@@ -1,60 +1,82 @@
 package com.company;
 
 public class DiceGame {
+    private boolean won = false;
     private int winCount;
+    private int loseCount;
 
     public int getWinCount() {
         return winCount;
     }
 
-    public void play(int simulations, Dice dice){
-        if(simulations <= 0){
+    public int getLoseCount() {
+        return loseCount;
+    }
+
+    public void play(int simulations, Dice dice) {
+        if (simulations <= 0) {
             System.out.println("Can't play. Nr. of simulations needs to be > 0");
             return;
         }
 
-        while(simulations > 0){
+        while (simulations > 0) {
             System.out.println("Simulation nr: " + simulations);
-            for(int i=0; i<4;i++){
+            won = false;
+            for (int i = 0; i < 4; i++) {
                 System.out.println("Roll number: " + (i + 1));
                 dice.roll();
                 int rolledValue = dice.getValueAfterRoll();
-                if(rolledValue == 6){
+                if (rolledValue == 6) {
+                    won = true;
                     winCount++;
-                    System.out.println("Win! New Win count: " + getWinCount());
+                    System.out.println("Win! Win count: " + getWinCount() + "$");
                     break;
                 }
             }
+            if (!won) {
+                loseCount++;
+                System.out.println("You lost! Lost count: " + getLoseCount() + "$");
+
+            }
+
             simulations--;
-
-
         }
+        System.out.println("Final score: Won: " + getWinCount() + "$" + " Lost: " + getLoseCount() + "$");
     }
 
-    public void play(int simulations, Dice dice1, Dice dice2){
-        if(simulations <= 0){
+    public void play(int simulations, Dice dice1, Dice dice2) {
+        if (simulations <= 0) {
             System.out.println("Can't play. Nr. of simulations needs to be > 0");
             return;
         }
 
-        while(simulations > 0){
+        while (simulations > 0) {
             System.out.println("Simulation nr: " + simulations);
-            for(int i=0; i<24;i++){
+            won = false;
+            for (int i = 0; i < 24; i++) {
                 System.out.println("Roll number: " + (i + 1));
                 dice1.roll();
                 dice2.roll();
                 int rolledValue1 = dice1.getValueAfterRoll();
                 int rolledValue2 = dice2.getValueAfterRoll();
 
-                if(rolledValue1 == 6 && rolledValue2 == 6){
+                if (rolledValue1 == 6 && rolledValue2 == 6) {
+                    won = true;
                     winCount++;
-                    System.out.println("Win! New Win count: " + getWinCount());
+                    System.out.println("Win! New Win count: " + getWinCount() + "$");
                     break;
                 }
             }
+            if(!won){
+                loseCount++;
+                System.out.println("You lost! Lost count: " + getLoseCount()+ "$");
+            }
+
             simulations--;
 
 
         }
+
+        System.out.println("Final score: Won: " + getWinCount() + "$" + " Lost: " + getLoseCount() + "$");
     }
 }
